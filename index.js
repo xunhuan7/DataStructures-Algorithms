ecfadb11aca6ae860ba8dc6cc3600ee549dd3ec2
jason.deng <jason.deng@ximalaya.com>
2017-05-25 10:31:53 +0800

    新增服务代码

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dengqiyao on 2017/5/25.
+ */
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8888);
