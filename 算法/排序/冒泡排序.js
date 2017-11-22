73eedd06ef949d0145164e448faa156ca5eb0db0
Jason.Deng <676081195@qq.com>
2017-11-22 11:13:23 +0800

    完善冒泡排序

--- 算法/排序/冒泡排序.js
+++ 算法/排序/冒泡排序.js
@@ -5,12 +5,17 @@
 function bubbleSort(arr) {
     var len = arr.length;
     for (var i = 0; i < len - 1; i++) {
+        var is_sort = true;
         for (var j = 0; j < len - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
+                is_sort = false;
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+        }
+        if (is_sort) {
+            return arr;
         }
     }
     return arr;
